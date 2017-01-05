b6e8ce2a1e452bcb2cf24b5c1b2a6e9bac7259fb
David Bal <david.bal37@gmail.com>
2017-01-05 15:33:14 -0800

    change to int as key

--- src/net/balsoftware/resources/RRuleResource.java
+++ src/net/balsoftware/resources/RRuleResource.java
@@ -1,9 +1,9 @@
 package net.balsoftware.resources;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,16 +22,17 @@ public class RRuleResource {
 	
 	@POST
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	@Produces(MediaType.TEXT_PLAIN)
-	public String doPost(RRuleBean request)
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<String> doPost(RRuleBean request)
 	{
-//		DateTimeStart dateTimeStart = DateTimeStart.parse("DTSTART:20170104T131901");
-//		
+		DateTimeStart dateTimeStart = DateTimeStart.parse(request.getDtstartContent());
+//		int limit = Integer.parseInt(request.getMaxRecurrences());
+		int limit = request.getMaxRecurrences();
 //		String recurrences;
 //		try {
-//		RecurrenceRule rrule = RecurrenceRule.parse("RRULE:FREQ=DAILY");
+//		RecurrenceRule rrule = RecurrenceRule.parse(request.getRruleContent());
 //		recurrences = rrule.getValue().streamRecurrences(dateTimeStart.getValue())
-//				.limit(10)
+//				.limit(limit)
 //				.map(t -> t.toString())
 //				.collect(Collectors.joining(LS));
 //		} catch (Exception e)
@@ -40,92 +41,17 @@ public class RRuleResource {
 //		}
 //		return recurrences;
 		
-		DateTimeStart dateTimeStart = DateTimeStart.parse(request.getDtstartContent());
-		int limit = Integer.parseInt(request.getMaxRecurrences());
-//		int limit = request.getMaxRecurrences();
-		String recurrences;
+		List<String> recurrences;
 		try {
 		RecurrenceRule rrule = RecurrenceRule.parse(request.getRruleContent());
 		recurrences = rrule.getValue().streamRecurrences(dateTimeStart.getValue())
 				.limit(limit)
 				.map(t -> t.toString())
-				.collect(Collectors.joining(LS));
+				.collect(Collectors.toList());
 		} catch (Exception e)
 		{
-			recurrences = "Invalid";
+			recurrences = null;
 		}
 		return recurrences;
 	}
-	
-	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
-	@Produces(MediaType.TEXT_PLAIN)
-	public String doPost(String request)
-	{
-		return request;
-	}
-	
-//	@POST
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	@Produces(MediaType.TEXT_PLAIN)
-//	protected String doPost()
-//	{
-//		return "here";
-////		System.out.println("run servlet");
-////		DateTimeStart dateTimeStart = DateTimeStart.parse(request.getDtstartContent());
-////		
-////		String recurrences;
-////		try {
-////		RecurrenceRule rrule = RecurrenceRule.parse(request.getRruleContent());
-////		recurrences = rrule.getValue().streamRecurrences(dateTimeStart.getValue())
-////				.limit(request.getMaxRecurrences())
-////				.map(t -> t.toString())
-////				.collect(Collectors.joining(LS));
-////		} catch (Exception e)
-////		{
-////			recurrences = "Invalid";
-////		}
-////		return recurrences;
-//	}
-	
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String doGet()
-	{
-		DateTimeStart dateTimeStart = DateTimeStart.parse("DTSTART:20170104T131901");
-		
-		String recurrences;
-		try {
-		RecurrenceRule rrule = RecurrenceRule.parse("RRULE:FREQ=DAILY");
-		recurrences = rrule.getValue().streamRecurrences(dateTimeStart.getValue())
-				.limit(10)
-				.map(t -> t.toString())
-				.collect(Collectors.joining(LS));
-		} catch (Exception e)
-		{
-			recurrences = "Invalid";
-		}
-		return recurrences;
-	}
-
-//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		// get 1st row in database as initial setting
-//	}
-
-//	/**
-//     * @see HttpServlet#HttpServlet()
-//     */
-//    public RRuleServlet3() {
-//        super();
-//    }
-//
-//	/**
-//	 * @see Servlet#init(ServletConfig)
-//	 */
-//	@Override
-//	public void init(ServletConfig config) throws ServletException {
-//		// TODO Auto-generated method stub
-//	}
-
 }
